bda9a1d04f8340a420f1e5e84b035336f19a0740
henriquePinheiro <henriquepinheiro18@gmail.com>
2022-10-01 13:30:27 -0300

    Inserir AVL

--- AVL/main.c
+++ AVL/main.c
@@ -5,6 +5,7 @@ typedef struct sNodo {
   struct sNodo *right;
   struct sNodo *left;
   struct sLivro *livro;
+  int height;
 } Nodo;
 
 typedef struct sLivro {
@@ -25,6 +26,13 @@ Nodo *remover1Filho(Nodo *, Nodo *);
 Nodo *verificarFilhos(Nodo *);
 Nodo *remover2Filhos(Nodo *, Nodo *);
 Nodo* removerTudo(Nodo*);
+int maior(int,int);
+Nodo * minValueNode( Nodo*);
+int getBalance(Nodo *n);
+Nodo *leftRotate(Nodo *);
+Nodo *rightRotate(Nodo  *);
+int height(Nodo *n);
+Nodo* verificarRotacao(Nodo*, int);
 
 int main() {
   Nodo *root = NULL;
@@ -45,16 +53,77 @@ int main() {
 
   percorrerInOrder(root);
   printf("\n\n");
-  root = verificarRemover(root, 12);
-  root = verificarRemover(root, 7);
-  root = verificarRemover(root, 11);
-  root = verificarRemover(root, 2);
-  root = verificarRemover(root, 5);
+  // root = verificarRemover(root, 12);
+  // root = verificarRemover(root, 7);
+  // root = verificarRemover(root, 11);
+  // root = verificarRemover(root, 2);
+  // root = verificarRemover(root, 5);
 
-  percorrerPosOrdem(root);
-  root =  removerTudo(root);
-  percorrerPosOrdem(root);
+  percorrerPreOrdem(root);
+  // root =  removerTudo(root);
+  // percorrerPreOrdem(root);
   return 0;
+}
+
+int maior(int a, int b)
+{
+    return (a > b)? a : b;
+}
+
+int height(Nodo *n){
+    if (n == NULL)
+    return 0;
+    return n->height;
+}
+
+Nodo *rightRotate(Nodo  *y) {
+    Nodo *x = y->left;
+    Nodo *aux = x->right;
+ 
+    // Perform rotation
+    x->right = y;
+    y->left = aux;
+ 
+    // Update heights
+    y->height = maior(height(y->left), height(y->right))+1;
+    x->height = maior(height(x->left), height(x->right))+1;
+ 
+    // Return new root
+    return x;
+}
+
+Nodo *leftRotate(Nodo *x){
+    Nodo *y = x->right;
+    Nodo *aux = y->left;
+ 
+    // Perform rotation
+    y->left = x;
+    x->right = aux;
+ 
+    //  Update heights
+    x->height = maior(height(x->left), height(x->right))+1;
+    y->height = maior(height(y->left), height(y->right))+1;
+ 
+    // Return new root
+    return y;
+}
+
+int getBalance(Nodo *n)
+{
+    if (n == NULL)
+        return 0;
+    return height(n->left) - height(n->right);
+}
+
+ Nodo * minValueNode( Nodo* node)
+{
+     Nodo* current = node;
+ 
+    /* loop down to find the leftmost leaf */
+    while (current->left != NULL)
+        current = current->left;
+ 
+    return current;
 }
 
 Nodo *criarNodo(int codigo) { // aloca memória para o nodo
@@ -62,7 +131,7 @@ Nodo *criarNodo(int codigo) { // aloca memória para o nodo
   nodo->livro = criarLivro(codigo);
   nodo->left = NULL;
   nodo->right = NULL;
-
+  nodo->height =1;
   return nodo;
 }
 
@@ -72,23 +141,55 @@ Livro *criarLivro(int codigo) { // aloca memória para o livro
   return livro;
 }
 
-Nodo *inserir(Nodo *nodo,
-              int codigo) { // encontra a posição e insere o elemento
+Nodo *inserir(Nodo *nodo, int codigo) { // encontra a posição e insere o elemento
   if (nodo == NULL) {
     return criarNodo(codigo);
-  } else {
+  } else if(nodo!=NULL){
     if (nodo->livro->codigo > codigo) {
       nodo->left = inserir(nodo->left, codigo);
     } else if (nodo->livro->codigo <= codigo) {
       nodo->right = inserir(nodo->right, codigo);
     }
+  } else{
+      return nodo;
   }
-  return nodo;
+
+  nodo->height = 1 + maior(height(nodo->left), height(nodo->right));
+
+  int balance = getBalance(nodo);
+  nodo = verificarRotacao(nodo, balance);
+  // Left Left Case
+   
+ 
+    return nodo;
+}
+
+Nodo* verificarRotacao(Nodo* nodo, int balance){
+   if (balance > 1 && getBalance(nodo->left) >= 0)
+        return rightRotate(nodo);
+ 
+    // Left Right Case
+    if (balance > 1 && getBalance(nodo->left) < 0)
+    {
+        nodo->left =  leftRotate(nodo->left);
+        return rightRotate(nodo);
+    }
+ 
+    // Right Right Case
+    if (balance < -1 && getBalance(nodo->right) <= 0)
+        return leftRotate(nodo);
+ 
+    // Right Left Case
+    if (balance < -1 && getBalance(nodo->right) > 0)
+    {
+        nodo->right = rightRotate(nodo->right);
+        return leftRotate(nodo);
+    }
 }
 
 void percorrerPreOrdem(Nodo *nodo) {
   if (nodo != NULL) {
-    printf("%i \n ", nodo->livro->codigo);
+    printf("Valor: %i Height: %i \n ", nodo->livro->codigo, nodo->height);
     percorrerPreOrdem(nodo->left);
     percorrerPreOrdem(nodo->right);
   }
@@ -181,6 +282,7 @@ Nodo *remover1Filho(Nodo *nodo, Nodo *aux) {
   }
   return aux;
 }
+
 void verificarErro(int erro) {
   if (erro == -1) {
     printf("Livro não existe\n");
